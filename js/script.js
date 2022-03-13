076c1989af1e25b1a5d8e360403ba1905e38b5c8
Sonu Dutta <itsmesonudutta@gmail.com>
2022-03-13 10:51:44 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+
+let flag=1;
+
+function validateForm(){
+    if(username.value==""){
+        document.getElementById("userError").innerHTML="Username cannot be empty!";
+        flag=0;
+    }
+    else if(username.value.length<6){
+        document.getElementById("userError").innerHTML="Username should be atleast 6 characters!";
+        flag=0;
+    }
+    else{
+        document.getElementById("userError").innerHTML="";
+        flag=1;
+    }
+    if(password.value==""){
+        document.getElementById("passError").innerHTML="Password cannot be empty!";
+        flag=0;
+    }
+    else{
+        document.getElementById("passError").innerHTML="";
+        flag=1;
+    }
+    if(flag){
+        return true;
+
+    }else{
+        return false;
+    }
+
+}
